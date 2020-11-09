40bc0a302569c8892ca5f0fb0e9b562ee7010259
HakunaMatata1209 <susmitha.begari@gmail.com>
2020-11-09 10:23:07 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import CodeChallange from "./Components/CodeChallange";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>
+        Displaying 32,768 unique colors in single canvas with Rgb colors between
+        0 to 256 using multiplication of 8 till 256
+      </h2>
+      <CodeChallange></CodeChallange>
     </div>
   );
 }
---
+++ src/Components/CodeChallange.js
@@ -0,0 +1,101 @@
+import React, { useRef, useEffect } from "react";
+
+function CodeChallange() {
+  //creating canvas referance
+  const myCanvas = useRef(null);
+  //multiplying by 8 still 256
+  const number = 8;
+  //variable to make loop to execute
+  let value = 0;
+  //vaiable to hold current multiplication number
+  let result;
+
+  /*Diving the components into 3 steps...0 to 256
+    //if step one chnage the first key r valuein a object from 8 to 256
+    //if step two chnage the second key g value in a object from 8 to 256
+    //if step one chnage the first key b value in a object from 8 to 256
+     looping throughg 356 * 128 to get 32 768 values
+
+    
+    */
+  useEffect(() => {
+    const canvasContent = myCanvas.current;
+    const context = canvasContent.getContext("2d");
+    context.canvas.width = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+
+    for (let step = 0; step <= 3; step++) {
+      const stepValue = step;
+
+      for (let i = 1; i <= 32; i++) {
+        const result = i * number;
+        for (let column = 0; column < 256; column++) {
+          for (let row = 0; row < 128; row++) {
+            if (stepValue == 1) {
+              
+              value++;
+              // console.log("step 1"+stepValue + "the result"+result)
+              context.fillStyle =
+                "rgb(" +
+                result +
+                "," +
+                Math.floor(Math.random() * 256) +
+                "," +
+                Math.floor(Math.random() * 256) +
+                ")";
+              // console.log("rgb(" + result + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")" );
+            }else if(stepValue == 2){ 
+             
+               
+              value++;
+              context.fillStyle =
+                "rgb(" +
+                Math.floor(Math.random() * 256) +
+                "," +
+                result +
+                "," +
+                Math.floor(Math.random() * 256) +
+                ")";
+              // console.log("rgb(" + Math.floor(Math.random() * 256) + "," + result +"," + Math.floor(Math.random() * 256) + ")" );
+            } else {
+              //console.log("while 3")
+              value++;
+              context.fillStyle =
+                "rgb(" +
+                Math.floor(Math.random() * 256) +
+                "," +
+                Math.floor(Math.random() * 256) +
+                "," +
+                result +
+                ")";
+              // console.log("rgb(" +Math.floor(Math.random() * 256) +"," + Math.floor(Math.random() * 256) +"," + result +")" );
+            }
+
+            //context.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) +   "," +Math.floor(Math.random() * 256) +")";
+            context.fillRect(column * 20, row * 20, 20, 20);
+            value++;
+          }
+          value++;
+        }
+
+        if (result > 256) {
+          break;
+        }
+        value++;
+      }
+      value++;
+    }
+
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <canvas ref={myCanvas}></canvas>
+      </div>
+    </div>
+  );
+}
+
+export default CodeChallange;
